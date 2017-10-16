8c883b2de69f843b343a558bfe76603f30f95afe
Mary Benitez <meebenitez@gmail.com>
2017-10-16 06:55:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/newteam" do
+   erb :newteam
+ end
+
+ post '/team' do
+   erb :team
+ end
 
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -6,7 +6,7 @@ class Puppy
     @breed = breed
     @age = age
   end
-  
+
 
 
 
